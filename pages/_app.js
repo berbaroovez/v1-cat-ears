a07dad49a29ef122048ee57c79cecd5393833222
berbaroovez <carterolson3@gmail.com>
2021-05-26 16:27:25 -0500

    tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import GlobalStyles from "../util/GlobalStyles";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
+import * as Fathom from "fathom-client";
+
+import { useEffect } from "react";
+
+Router.events.on("routeChangeComplete", () => {
+  Fathom.trackPageview();
+});
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      Fathom.load(process.env.FATHOM_SITE_ID, {
+        includedDomains: ["https://www.catearsforv1.com/"],
+      });
+    }
+  }, []);
+
   return (
     <>
       <GlobalStyles />
